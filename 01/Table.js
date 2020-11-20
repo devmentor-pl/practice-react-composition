2d1ede24d0732e950292dd2436c41c85997b813d
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-11-20 20:30:20 +0100

    Show data in the table component

--- 01/Table.js
+++ 01/Table.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React from "react";
 
 class Table extends React.Component {
-    render() {
-        const {data} = this.props;
-        console.log(data);
+  render() {
+    const { data } = this.props;
+    const list = data.map((item) => {
+      return (
+        <tr key={item.id}>
+          <td>{item.name}</td>
+          <td>{item.price}</td>
+          <td>{item.quantity}</td>
+          <td>{item.price * item.quantity}</td>
+        </tr>
+      );
+    });
 
-        return <table></table>
-    }
+    const totalPrice = data.reduce((price, item) => {
+      return price + item.price * item.quantity;
+    }, 0);
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total Price</th>
+          </tr>
+        </thead>
+        <tbody>{list}</tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="3">Total</td>
+            <td>{totalPrice}</td>
+          </tr>
+        </tfoot>
+      </table>
+    );
+  }
 }
 
 export default Table;
