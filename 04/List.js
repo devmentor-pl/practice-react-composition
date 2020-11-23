9df4cf8ee9f86417f49c6e485a28381cacea2156
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-11-23 23:02:39 +0100

    Finish task 4

--- 04/List.js
+++ 04/List.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
 
 class List extends React.Component {
-    render() {
-        return <ul>Lista plików</ul>
-    }
+  render() {
+    const listItems = this.props.items.map((item) => {
+      return (
+        <li key={item.id}>
+          <h2>File name: {item.name}</h2>
+          <p>File size: {item.size}</p>
+          <h3>File content:</h3>
+          <p>{item.content}</p>
+        </li>
+      );
+    });
+
+    return (
+      <>
+        <h1>Lista plików</h1>
+        <ul>{listItems}</ul>
+      </>
+    );
+  }
 }
 
 export default List;
