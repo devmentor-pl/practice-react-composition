d46a6c66e738405043c68a8ea14330cbfc0269a0
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-11-24 00:01:18 +0100

    Finish task 5

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
 
 class Textarea extends React.Component {
-    render() {
-        return <textarea></textarea>
+  extraHeightOffset = 20;
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    const { offsetHeight } = this.textArea;
+    const shouldResize = offsetHeight + this.extraHeightOffset < 100;
+
+    return {
+      resize: shouldResize,
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    const { offsetHeight, scrollHeight } = this.textArea;
+    const shouldResize = offsetHeight < scrollHeight;
+
+    if (shouldResize && snapshot.resize) {
+      this.textArea.style.height = offsetHeight + this.extraHeightOffset + "px";
     }
+  }
+
+  render() {
+    const { content } = this.props;
+
+    return (
+      <textarea
+        ref={(node) => (this.textArea = node)}
+        value={content}
+        onChange={(e) => this.props.onTextChange(e.target.value)}
+      ></textarea>
+    );
+  }
 }
 
 export default Textarea;
