fa848e3952deb4b10020a462a4f162184e4b5f4e
Martin Gawlyta <martin.guide@gmail.com>
2020-12-07 22:31:17 +0100

    Add table structure

--- 01/Table.js
+++ 01/Table.js
@@ -3,10 +3,55 @@ import React from 'react';
 class Table extends React.Component {
     render() {
         const {data} = this.props;
-        console.log(data);
+        const headerList = Object.keys(data[0]).map(key => {
 
-        return <table></table>
+            return (
+                <td key={key}>{key}</td>
+            )
+        })
+
+        const tableRow = data.map(row => {
+            return (
+                <tr key={row.id}>
+                    <td>{row.id}</td>
+                    <td>{row.name}</td>
+                    <td>{row.price}</td>
+                    <td>{row.quantity}</td>
+                </tr>
+            )
+        })
+
+        const totalSum = data.reduce((acc, item) => {
+            return acc + item.price * item.quantity}, 0)
+
+        return (
+        <table style={tableStyle}>
+            <thead>
+                <tr>
+                    {headerList}
+                </tr>
+            </thead>
+
+            <tbody>
+                    {tableRow}
+            </tbody>
+
+            <tfoot>
+                <tr>
+                    <td>
+                        {totalSum}
+                    </td>
+                </tr>
+            </tfoot>
+
+        </table>
+        )
     }
+}
+
+const tableStyle = {
+    "border": "1px solid black",
+    "borderSpacing": "10px",
 }
 
 export default Table;
