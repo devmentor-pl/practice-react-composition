4911c191f3bfacdc217237cdb808d2cfa49f5cf1
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2021-01-09 12:36:14 +0100

    Task 2 finished

--- 02/Form.js
+++ 02/Form.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 class Form extends React.Component {
-    render() {
-        return (
-            <section>
-                <input /><input type="submit" />
-            </section>
-        )
-    }
+  getInputValue() {
+    if (this.input.value) {
+      const value = this.input.value;
+      this.clearInput();
+      return value;
+    } else return null;
+  }
+
+  clearInput = () => {
+    this.input.value = '';
+  };
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <section>
+        <form onSubmit={handleSubmit}>
+          <input
+            defaultValue='Wpisz imię... '
+            ref={(input) => (this.input = input)}
+          />
+          <input type='submit' />
+        </form>
+      </section>
+    );
+  }
 }
 
 export default Form;
--- 02/List.js
+++ 02/List.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { v4 as uuid } from 'uuid';
 
 import ListItem from './ListItem';
 
-function List(props) {
-    return (
-        <ul>
-            <ListItem />
-        </ul>
-    )
-}
+const List = (props) => {
+  const { items } = props;
 
-export default ListItem;
+  const renderList = items.map((item) => {
+    return <ListItem name={item} key={uuid()} />;
+  });
+
+  return <ul>{renderList}</ul>;
+};
+
+export default List;
