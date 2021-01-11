2363ba2b64e751548a36839d47868445ed4add85
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2021-01-11 10:08:09 +0100

    Task no. 5 finished

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -4,11 +4,22 @@ class Textarea extends React.Component {
   getText() {
     return this.text.value;
   }
+
+  getOffsetHeight() {
+    return this.text.offsetHeight;
+  }
+
+  getScrollHeight() {
+    return this.text.scrollHeight;
+  }
+
   render() {
+    const { height } = this.props;
     return (
       <textarea
         ref={(el) => (this.text = el)}
         onChange={this.props.changed}
+        style={{ height: height }}
       ></textarea>
     );
   }
--- 05/app.js
+++ 05/app.js
@@ -6,19 +6,43 @@ import Textarea from './Textarea';
 class App extends React.Component {
   state = {
     text: '',
+    height: 16,
   };
 
   textUpdate = () => {
-    console.log(this.textRef.getText());
+    let textInTextArea = this.textRef.getText();
+    this.setState({ text: textInTextArea });
   };
 
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    let offsetHeight = this.textRef.getOffsetHeight();
+
+    if (offsetHeight < 100) {
+      return { resize: true };
+    } else {
+      return { resize: false };
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    let offsetHeight = this.textRef.getOffsetHeight();
+    let scrollHeight = this.textRef.getScrollHeight();
+
+    let heightCopy = this.state.height;
+
+    if (snapshot.resize && scrollHeight > offsetHeight) {
+      this.setState({ height: heightCopy + 8 });
+    }
+  }
+
   render() {
-    const { text } = this.state;
+    const { text, height } = this.state;
     return (
       <Textarea
         content={text}
         ref={(ref) => (this.textRef = ref)}
         changed={this.textUpdate}
+        height={height}
       />
     );
   }
