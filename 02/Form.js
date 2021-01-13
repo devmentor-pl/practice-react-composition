7d735391a6db3378c9d4351b539704c6eba4a013
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2021-01-13 10:07:21 +0100

    Fix task 02

--- 02/Form.js
+++ 02/Form.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 class Form extends React.Component {
   getInputValue() {
-    if (this.input.value) {
-      const value = this.input.value;
+    const { value } = this.input;
+    if (value) {
       this.clearInput();
       return value;
     } else return null;
