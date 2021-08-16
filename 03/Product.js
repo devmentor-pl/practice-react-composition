c8f815c8d08067a73ecb42b93ebdb843e7161fe9
Laura Zacharuk <laura.zacharuk@gmail.com>
2021-08-16 11:12:30 +0200

    task 4

---
+++ 03/Product.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Product(props) {
+    return <h2>Product</h2>
+}
+
+export default Product;
--- 03/app.js
+++ 03/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Category from './Category';
 import Cart from './Cart';
+import Product from './Product'
 
 import data from './data.json';
 
@@ -10,18 +11,60 @@ class App extends React.Component {
     state = {
         cart: [],
     }
-    
+
+    addProduct = product => {
+        product.isCart = true;
+        this.setState({ cart: [...this.state.cart, product] })
+    }
+
+    removeProduct = product => {
+        product.isCart = false;
+        const newAddedProduct = this.state.cart.filter(cartProduct => cartProduct.id !== product.id);
+        this.setState({ cart: newAddedProduct })
+    }
+
+
+    renderCategoryList() {
+        return data.map(product => {
+
+            return (
+                <Product
+                    key={product.id}
+                    onAdd={this.addProduct}
+                    isCategory={true}
+                    product={product} />
+            )
+        })
+    }
+
+
+    renderCartList() {
+        return this.state.cart.map(product => {
+            return (
+                <Product
+                    key={product.id}
+                    onRemove={this.removeProduct}
+                    isCategory={false}
+                    product={product} />
+            )
+        })
+    }
     render() {
         return (
             <section>
-                <Category />
-                <Cart />
+                <Category>
+                    {this.renderCategoryList()}
+                </Category>
+
+                <Cart>
+                    {this.renderCartList()}
+                </Cart>
             </section>
         )
     }
 }
 
 ReactDOM.render(
-    <App />, 
+    <App />,
     document.querySelector('#root')
 );
