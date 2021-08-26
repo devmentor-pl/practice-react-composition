72d7a2afe738742b6aeb47b5b44553fb23b2ba7d
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-08-26 20:01:33 +0200

    Task 04
    Task 04 August 26

--- 04/File.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class File extends React.Component {
-    render() {
-        return <input type="file" multiple />
-    }
-}
-
-export default File;
--- 04/List.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class List extends React.Component {
-    render() {
-        return <ul>Lista plików</ul>
-    }
-}
-
-export default List;
