26cee536e579c5bcd5f92502b99c81025a60dbfd
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-08-27 00:13:04 +0200

    Task 05
    Task 05 August 27

--- 05/app.js
+++ 05/app.js
@@ -6,13 +6,46 @@ import Textarea from './Textarea';
 class App extends React.Component {
     state = {
         text: '',
+        height: 20,
+    };
+
+    textUpdate = () => {
+        let textAreaContent = this.textRef.getTextAreaContent();
+        this.setState({text: textAreaContent});
+    };
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+
+        let offsetHeight = this.textRef.getOffsetHeight();
+
+        if (offsetHeight <= 100) {
+            console.log(offsetHeight);
+            return {resize: true};
+        } else {
+            return {resize: false};
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+
+        let offsetHeight = this.textRef.getOffsetHeight();
+        let scrollHeight = this.textRef.getScrollHeight();
+
+        if (scrollHeight > offsetHeight && snapshot.resize === true) {
+            this.setState({height: this.state.height + 10});
+        }
     }
 
     render() {
-        const { text } = this.state;
+        const {text, height} = this.state;
         return (
-            <Textarea content={ text } />
-        )
+            <Textarea
+                content={text}
+                ref={(ref) => (this.textRef = ref)}
+                changed={this.textUpdate}
+                height={height}
+            />
+        );
     }
 }
 
