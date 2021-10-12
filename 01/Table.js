6f4fbfedd95f20089f96b18e2b8ceb7845f6cdac
Kowacka Malgorzata <kowackag@gmail.com>
2021-10-12 11:16:28 +0200

    Finish Task 01

--- 01/Table.js
+++ 01/Table.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import TableHeader from './components/TableHeader';
+import TableFooter from './components/TableFooter';
+import TableBody from './components/TableBody';
 
 class Table extends React.Component {
     render() {
         const {data} = this.props;
-        console.log(data);
-
-        return <table></table>
+        const {id, ...rest} = data[0];
+        const titles = Object.keys(rest);
+        const totalPrice = data.reduce((acc,cur) => acc+ cur.price * cur.quantity, 0);
+        return (
+            <table>
+                <TableHeader titles={titles}/>
+                <TableBody item={data}/>
+                <TableFooter price={totalPrice}/>
+            </table>
+        )
     }
 }
 
