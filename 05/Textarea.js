720742cc32ba2376ff08b68b9c92f8c6f01ddc1e
Kowacka Malgorzata <kowackag@gmail.com>
2021-10-14 15:00:20 +0200

    Finish Task 05

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
 class Textarea extends React.Component {
+    
+    getHeight() {
+        return this.textarea.offsetHeight;
+    }
+
+    getScrollHeight() {
+        return this.textarea.scrollHeight;
+    }
+
+    handleInput = (e) =>  {
+         e.preventDefault();
+         this.props.onChange(this.textarea.value)
+     }   
+
+
     render() {
-        return <textarea></textarea>
+        return <textarea ref = {item =>this.textarea = item} style = {this.props.style} onChange={this.handleInput}></textarea>
     }
 }
 
