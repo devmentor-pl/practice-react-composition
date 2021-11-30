37ea059080a59ffc037305a3df17fbccee75fdd9
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-11-30 20:22:43 +0100

    closes task 5

--- 05/app.js
+++ 05/app.js
@@ -6,14 +6,50 @@ import Textarea from './Textarea';
 class App extends React.Component {
     state = {
         text: '',
-    }
+        textareaHeight: 'inherit'
+    };
+
+    getSnapshotBeforeUpdate() {
+        return this.teaxtarea.getTextareaHeight() < 100 ?
+            { resize: true } :
+            { resize: false };
+    };
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (snapshot.resize) {
+            const textareaHeight = this.teaxtarea.getTextareaHeight();
+            let scrollHeight = this.teaxtarea.getScrollHeight();
+            if (textareaHeight < scrollHeight) {
+                this.resizeTextarea(scrollHeight);
+            };
+        };
+    };
+
+    resizeTextarea(height) {
+        this.setState({
+            textareaHeight: `${height}px`
+        });
+    };
+
+    setTextState = () => {
+        const content = this.teaxtarea.getContent();
+        this.setState({
+            text: content
+        });
+    };
 
     render() {
-        const { text } = this.state;
+        const { text, textareaHeight } = this.state;
+        const style = { height: textareaHeight }
         return (
-            <Textarea content={ text } />
-        )
-    }
+            <Textarea
+                ref={el => this.teaxtarea = el}
+                content={text}
+                onChange={this.setTextState}
+                style={style}
+            />
+        );
+    };
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
