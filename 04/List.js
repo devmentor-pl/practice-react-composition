b85fdb8abb7f84b4d1fa15a664d93cfe1474568f
Jakub Chrobak <jchrobakk@gmail.com>
2021-12-10 22:02:17 +0100

    Render error

--- 04/List.js
+++ 04/List.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
 
 class List extends React.Component {
-    render() {
-        return <ul>Lista plików</ul>
-    }
+  render() {
+    const { filesList } = this.props;
+    console.log(filesList);
+    const files = filesList.map((file) => {
+      const { name, size, content, id } = file;
+      return (
+        <li key={id}>
+          <p>Name: {name}</p>
+          <p>Size: {size}</p>
+          <p>Content: {content}</p>
+        </li>
+      );
+    });
+
+    return <ul>{files}</ul>;
+  }
 }
 
 export default List;
