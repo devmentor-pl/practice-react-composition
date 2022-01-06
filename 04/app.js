9a9d98aead091e91c5967ffcb651edbf0c853d7f
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2022-01-06 20:40:57 +0100

    Task 04 - done

--- 04/app.js
+++ 04/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import File from './File';
 import List from './List';
+import { v4 as uuid } from 'uuid';
 
 class App extends React.Component {
     state = {
@@ -12,10 +13,37 @@ class App extends React.Component {
     render() {
         return (
             <section>
-                <File />
-                <List />
+                <File onChange={this.addFileDataToState}/>
+                <List data={this.state.filesList}/>
             </section>
         )
+    }
+
+    addFileDataToState = (data) => {
+        Object.values(data).map( file => {
+            if( file && file.type.includes('text') ) {
+
+                const reader = new FileReader();
+
+                reader.onload = (readerEvent) => {
+                    const fileData = {
+                        name: file.name,
+                        size: file.size,
+                        content: readerEvent.target.result,
+                        id: uuid()
+                    };
+
+                    this.setState( state => {
+                        return {
+                            filesList: [...state.filesList , fileData]
+                        }
+                    });
+                }
+                reader.readAsText(file, 'UTF-8')
+            } else {
+                alert('You must chose text files');
+            }
+        })
     }
 }
 
