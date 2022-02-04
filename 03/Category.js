4406734a482aa38751c72fd03fa7967c15d492fc
Rafał Kazik <kazik.raf@gmail.com>
2022-02-05 00:04:00 +0100

    3rd task

--- 03/Category.js
+++ 03/Category.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React from "react";
+import { v4 as uuid } from "uuid";
 
 function Category(props) {
-    return <h2>Category</h2>
+  const { items } = props;
+
+  console.log(items);
+
+  const categoryStyle = {
+    display: "flex",
+    flexDirection: "column",
+    width: "20vw",
+    border: "1px solid grey",
+    margin: "10px",
+    padding: "10px",
+  };
+
+  function addToCart(item) {
+    // console.log(item);
+    props.addItemToCart(item);
+  }
+
+  const categoryItems = props.items.map((item) => {
+    return (
+      <section key={uuid()} style={categoryStyle}>
+        <p>{item.name}</p>
+        <p> cena: {item.price}</p>
+        <button onClick={() => addToCart(item)}>Dodaj do koszyka</button>
+      </section>
+    );
+  });
+  return (
+    <>
+      <h2>Category</h2>
+      {categoryItems}
+    </>
+  );
 }
 
 export default Category;
--- 03/app.js
+++ 03/app.js
@@ -1,27 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Category from './Category';
-import Cart from './Cart';
+import Category from "./Category";
+import Cart from "./Cart";
 
-import data from './data.json';
+import data from "./data.json";
 
 class App extends React.Component {
-    state = {
-        cart: [],
-    }
-    
-    render() {
-        return (
-            <section>
-                <Category />
-                <Cart />
-            </section>
-        )
-    }
+  state = {
+    cart: [],
+  };
+
+  addItemToCart = (item) => {
+    const newItem = item;
+    this.setState((state) => {
+      return {
+        cart: [...state.cart, newItem],
+      };
+    });
+  };
+
+  removeItemfromCart = (item) => {
+    console.log(this.state.cart.indexOf(item));
+
+    const indexOfItemToRemove = this.state.cart.indexOf(item);
+    this.state.cart.splice(indexOfItemToRemove, 1);
+    this.setState({
+      cart: [...this.state.cart],
+    });
+  };
+
+  render() {
+    return (
+      <section>
+        <Category items={data} addItemToCart={this.addItemToCart} />
+        <Cart
+          cartItems={this.state.cart}
+          removeItemfromCart={this.removeItemfromCart}
+        />
+      </section>
+    );
+  }
 }
 
-ReactDOM.render(
-    <App />, 
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
