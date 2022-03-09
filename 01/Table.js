6c773a1d2c4f071395651b477fc149a1edef62c4
Ewelina <ewelina@ekopacz.pl>
2022-03-09 11:56:48 +0100

    created first version all is in Table component

--- 01/Table.js
+++ 01/Table.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 class Table extends React.Component {
+
+    renderProductName() {
+        const {data} = this.props;
+        return data.map(item => <tr>{item.name}</tr>)
+    }
+
+    renderQuantity(){
+        const {data} = this.props;
+        return data.map(item => <tr>{item.quantity}</tr>)
+    }
+
+    renderPrice(){
+        const {data} = this.props;
+        return data.map(item => <tr>{item.price}</tr>)
+    }
+
+    renderTotalPrice(){
+        const{data} = this.props;
+        return data.map(item => <tr>{item.price * item.quantity}</tr>)
+    }
+
     render() {
         const {data} = this.props;
         console.log(data);
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Produkt................................</th>
+                        <th>Ilość..................................</th>
+                        <th>Cena...................................</th>
+                        <th>Suma...................................</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{this.renderProductName()}</td>
+                        <td>{this.renderQuantity()}</td>
+                        <td>{this.renderPrice()}</td>
+                        <td>{this.renderTotalPrice()}</td>
+                    </tr>
 
-        return <table></table>
+                </tbody>
+                <tfoot><tr></tr></tfoot>
+            </table>
+        )
     }
 }
 
