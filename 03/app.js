868990970df462f3ef6dd0846d5d2b56fcde0c23
Jakub Parol <kubciop33@gmail.com>
2022-05-24 22:14:08 +0200

    Almost finished exercise 3

--- 03/app.js
+++ 03/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Category from './Category';
 import Cart from './Cart';
+import Product from './Product';
 
 import data from './data.json';
 
@@ -10,14 +11,51 @@ class App extends React.Component {
     state = {
         cart: [],
     }
+
     
     render() {
+        const {cart} = this.state;
         return (
             <section>
-                <Category />
-                <Cart />
+                <Category>
+                    {this.displayCategoryProducts(data)}
+                </Category>
+                <Cart>
+                    {this.displayCartProducts(cart)}
+                </Cart>
             </section>
         )
+    }
+
+    displayCategoryProducts(data) {
+        return data.map(item => <Product name={item.name} price={item.price} buttonText={'Dodaj do koszyka'} onClick={this.addItem} />)
+    }
+
+    displayCartProducts(data) {
+        return data.map(item => <Product name={item.name} price={item.price} buttonText={'Usuń z koszyka'} onClick={this.removeItem} />)
+    }
+
+    // Raczej nie powinienem tutaj operować na DOM ale wymiękam już nie mam pojęcia jak to inaczej ugryźć, podobnie z disabled
+
+    addItem = e => {
+        e.preventDefault();
+        const currItem = e.target.parentElement;
+        const name = currItem.firstElementChild.firstElementChild.textContent;
+        const price = currItem.children[1].textContent;
+        const item = {name, price};
+        this.setState({
+            cart: [...this.state.cart, item],
+        });
+    }
+
+    removeItem = e => {
+        e.preventDefault();
+        const currItem = e.target.parentElement;
+        const name = currItem.firstElementChild.firstElementChild.textContent;
+        const currItems = this.state.cart.filter(el => el.name !== name)
+        this.setState({
+            cart: currItems
+        });
     }
 }
 
