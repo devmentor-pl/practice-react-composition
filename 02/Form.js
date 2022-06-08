3fa27a356ebe4a80d3531e3bd641f09fadb0c5ab
Anna <krolikowska.ann@gmail.com>
2022-06-08 17:41:18 +0200

    02 added users list

--- 02/Form.js
+++ 02/Form.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 class Form extends React.Component {
+
     render() {
         return (
-            <section>
-                <input /><input type="submit" />
-            </section>
-        )
+            <form onSubmit={this.handleSubmit}>
+                <input ref={node => this.input = node} /><input type="submit"/>
+            </form>
+             )
+        }
     }
+    
+   handleSubmit = e => {
+        e.preventDefault()
+        console.log(this.input.value)
+
+        const {onSubmit} = this.props
+        onSubmit(this.input.value)
+        this.input.value = ''
 }
 
 export default Form;
