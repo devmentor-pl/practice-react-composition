4549a552ddf7888ae5f7335a125b751d90ea6905
Anna <krolikowska.ann@gmail.com>
2022-06-09 22:21:13 +0200

    04 added file reader

--- 04/List.js
+++ 04/List.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import {v4 as uuid} from 'uuid';
 
 class List extends React.Component {
     render() {
-        return <ul>Lista plików</ul>
+        return (
+            <ul>{this.renderList()}</ul>
+        )
+    }
+
+    renderList() {
+        const {data} = this.props
+
+        return data.map(file => {
+            return (
+                <li key={uuid()}>
+                    <h2>File name: {file.name}</h2>
+                    <p>File size: {file.size} B</p>
+                    <p>{file.content}</p>
+                </li>
+            )
+        })
     }
 }
 
--- 04/app.js
+++ 04/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 import File from './File';
 import List from './List';
 
@@ -12,11 +13,33 @@ class App extends React.Component {
     render() {
         return (
             <section>
-                <File />
-                <List />
+                <File onChange={this.readFile}/>
+                <List data={this.state.filesList} />
             </section>
         )
     }
-}
+
+    readFile = data => {
+        Object.values(data.target.files).map(file => {
+            if(file && file.type.includes('text')) { 
+                const reader = new FileReader()
+
+                const scope = this
+                reader.onload = function(readerEvent) {
+                    const content = readerEvent.target.result
+                    const newFile = {name: file.name, size: file.size, content: content}
+
+                    scope.setState(state =>{
+                       return{filesList: [...scope.state.filesList, newFile]}  
+                    })
+                }
+                reader.readAsText(file, 'UTF-8')
+            } else {
+                alert('Wybierz plik tekstowy!');
+                }
+            })     
+        }
+    }
+
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
