342f9e610403ae14be4b55e248078190829a16de
Anna <krolikowska.ann@gmail.com>
2022-06-09 20:09:15 +0200

    03 added cart, category, product

--- 03/app.js
+++ 03/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Category from './Category';
 import Cart from './Cart';
+import Product from './Product';
 
 import data from './data.json';
 
@@ -12,14 +13,47 @@ class App extends React.Component {
     }
     
     render() {
+
         return (
             <section>
-                <Category />
-                <Cart />
+                <Category>{this.renderCategory()}</Category>
+                <Cart>{this.renderCart()}</Cart> 
             </section>
         )
     }
+
+    renderCategory() {
+        return ( data.map(product => {
+                    return (
+                        <Product isCategory={true} key={product.id} name={product.name} disabled={this.state.cart.includes(product)} price={product.price} onClick= { () => this.addToCart(product)} btnText={'Dodaj do koszyka'}/>  //onClick={onClick}
+                    )
+                })
+    )}
+
+    renderCart() {   
+        return (
+            this.state.cart.map(product => {
+                return (
+                    <Product isCategory={false} key={product.id} name={product.name} price={product.price} onClick={() => this.removeFromCart(product.id)} btnText={'Usuń'}/>  
+                )
+            }
+        )
+    )}
+
+    addToCart = async(product)=> {
+        await this.setState(state => {
+            return {
+                cart: [...state.cart, product]
+            }
+        })
+    }
+
+    removeFromCart(id)  {
+        const currCart =  this.state.cart.filter(product => product.id !== id )
+        return this.setState({cart: currCart})    
+     }
 }
+
 
 ReactDOM.render(
     <App />, 
