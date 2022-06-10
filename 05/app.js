fdf88b349f44a89c62bc063919e1a70bcc785415
Anna <krolikowska.ann@gmail.com>
2022-06-10 16:31:48 +0200

    05 added textarea

--- 05/app.js
+++ 05/app.js
@@ -11,8 +11,33 @@ class App extends React.Component {
     render() {
         const { text } = this.state;
         return (
-            <Textarea content={ text } />
+            <Textarea content={ text } ref={ref => this.refText = ref}
+            getContent={this.getContent} />
         )
+    }
+
+    getContent = content => {
+        this.setState({
+            text: content 
+        })
+    }
+
+    getSnapshotBeforeUpdate() {
+        return {
+            height: this.refText.offsetHeight,
+            scrollHeight: this.refText.scrollHeight
+        }
+    }
+
+    componentDidUpdate() {
+
+        const textHight = this.refText.getHeight()
+        const textScrollHeight = this.refText.getScrollHeight()
+     
+        if(textHight < textScrollHeight) {
+            this.refText.refText.style.height = textScrollHeight + 'px';
+            this.refText.refText.style.maxHeight = '100px';
+        } 
     }
 }
 
