32ebd40cbbb38f5735ca021d4da463a8d7c7d515
Filip <43039909+Urfisior@users.noreply.github.com>
2022-07-24 20:58:14 +0100

    task 04 done

--- 04/File.js
+++ 04/File.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
 class File extends React.Component {
+    handleTxtFiles = (e) => {
+        const { getDataTxtFile } = this.props;
+        e.preventDefault();
+        const file = this.input.files[0];
+        console.log(file);
+        if (file && file.type.includes('text')) {
+            const reader = new FileReader();
+            reader.onload = (readerEvent) => {
+                const content = readerEvent.target.result;
+                getDataTxtFile(file.name, file.size, content);
+
+            };
+            reader.readAsText(file,'UTF-8')
+        } else {
+            console.log('Choose right file');
+        }
+     };
     render() {
-        return <input type="file" multiple />
+        return (
+            <form onSubmit={this.handleTxtFiles}>
+                <input ref={node => this.input = node} type="file" multiple/>
+                <input type="submit"/>
+            </form>
+        )
     }
 }
 
--- 04/List.js
+++ 04/List.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 class List extends React.Component {
     render() {
-        return <ul>Lista plików</ul>
+        const { data } = this.props;
+        return (
+            <ul>
+                {data.map(element => {
+                    return <li key={uuidv4()}>
+                                <header>{element.name}</header>
+                                <span>sieze: {element.size}</span>
+                                <p>{element.txt}</p>
+                            </li>})}
+            </ul>
+        )
     }
 }
 
