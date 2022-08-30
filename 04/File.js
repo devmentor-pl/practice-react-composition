575932c60c3946f0f700f5a792e3eabb3b2c417e
Mateusz Bogolubow <mateusz@bogolubow.com>
2022-08-30 17:49:33 +0200

    Finish 04

--- 04/File.js
+++ 04/File.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 class File extends React.Component {
+
+    handleChange = (e) => {
+        console.log(this.input.files);
+
+        [...this.input.files].forEach(file => {
+            if(file.type.includes('text')) {
+                const reader = new FileReader();
+                reader.addEventListener("load", () => {
+                    const data = {
+                        name: file.name,
+                        size: file.size,
+                        content: reader.result,
+                        id: uuidv4(),
+                    }
+                    
+                    this.props.addFile(data);
+                }, false);
+
+                reader.readAsText(file);
+            }
+        })
+    }
+
     render() {
-        return <input type="file" multiple />
+        return <input onChange={this.handleChange} ref={element => this.input = element} type="file" multiple />
     }
 }
 
--- 04/List.js
+++ 04/List.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 class List extends React.Component {
     render() {
-        return <ul>Lista plików</ul>
+        const {items} = this.props;
+        return <ul>
+            {items.map(({id, name, size, content}) => <li key={id}>{name}: {content} ({size})</li>)}
+        </ul>
     }
 }
 
