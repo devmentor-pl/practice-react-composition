ff21521c9f700b158d8f267d012f8cb3126c73f5
Mateusz Suplewski <matx3582@gmail.com>
2022-08-30 20:01:54 +0200

    task-5 Done

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from 'react'
 
 class Textarea extends React.Component {
-    render() {
-        return <textarea></textarea>
+  getSnapshotBeforeUpdate () {
+    return { resize: true }
+  }
+
+  componentDidUpdate (prevProps, prevState, snapshot) {
+    if (this.textArea.scrollHeight > this.textArea.offsetHeight && snapshot.resize === true) {
+      this.textArea.style.height = this.textArea.scrollHeight + 'px'
     }
+  }
+
+  render () {
+    const { updateText } = this.props
+    return <textarea ref={node => this.textArea = node} onChange={() => updateText(this.textArea.value)}></textarea>
+  }
 }
 
-export default Textarea;
+export default Textarea
--- 05/app.js
+++ 05/app.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import Textarea from './Textarea';
+import Textarea from './Textarea'
 
 class App extends React.Component {
-    state = {
-        text: '',
-    }
+  state = {
+    text: ''
+  }
 
-    render() {
-        const { text } = this.state;
-        return (
-            <Textarea content={ text } />
-        )
-    }
+  updateText = text => {
+    this.setState({ text })
+  }
+
+  render () {
+    const { text } = this.state
+    return (
+      <Textarea content={ text } updateText={this.updateText}/>
+    )
+  }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App/>, document.querySelector('#root'))
