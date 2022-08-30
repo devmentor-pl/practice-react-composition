60f654a607a7cd4b216f385c1fe0db0e822fe7c8
Mateusz Bogolubow <mateusz@bogolubow.com>
2022-08-30 17:25:24 +0200

    Finish 03

--- 03/Cart.js
+++ 03/Cart.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Cart(props) {
-    return <h2>Cart</h2>
+    return (
+        <section>
+            <h2>Cart</h2>
+            <ul>
+                {props.children}
+            </ul>
+        </section>
+        
+    );
 }
 
 export default Cart;
