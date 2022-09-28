6df017bdbade9589397dd36cbc70c6431b6fbbee
Katarzyna Panz <katarzyna.panz@gmail.com>
2022-09-28 09:57:44 +0200

    Add solution to exercise 05

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,8 +1,34 @@
 import React from 'react';
 
 class Textarea extends React.Component {
+
+    changeHandler = (e) => {
+        e.preventDefault();
+        const {add} = this.props;
+        const text = e.target.value;
+        add(text);
+    }
+
+    getHeight() {
+        return this.textarea.offsetHeight;
+        }
+
+    getScrollHeight() {
+        return this.textarea.scrollHeight;
+    }
+
+    setHeight() {
+        const {addRows} = this.props;
+        addRows();
+    }
+       
+
     render() {
-        return <textarea></textarea>
+        const {rows} = this.props;
+        return (
+                <textarea  rows={rows} ref={el => this.textarea = el} onChange={this.changeHandler}>
+                </textarea>
+        )
     }
 }
 
