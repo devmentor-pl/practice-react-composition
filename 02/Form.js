dc92a297c60ad2e26dee9928d043011f8810f470
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-10-13 20:15:24 +0200

    Finish 02

--- 02/Form.js
+++ 02/Form.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
 class Form extends React.Component {
+    refInput = React.createRef()
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const {addItem} = this.props;
+        const input = this.refInput.current;
+
+        if(typeof addItem === 'function') {
+            this.props.addItem(input.value);
+            input.value = '';
+        } else {
+            console.error('Nie przekazałeś funkcji do komponentu!');
+        }
+    }
+
     render() {
         return (
-            <section>
-                <input /><input type="submit" />
-            </section>
+            <form onSubmit={this.handleSubmit}>
+                <input ref={ this.refInput }/><input type="submit" />
+            </form>
         )
     }
 }
--- 02/List.js
+++ 02/List.js
@@ -3,9 +3,11 @@ import React from 'react';
 import ListItem from './ListItem';
 
 function List(props) {
+    const {items} = props;
+
     return (
         <ul>
-            <ListItem />
+            {items.map((i,index) => <ListItem key={index} name={i}/>)}
         </ul>
     )
 }
