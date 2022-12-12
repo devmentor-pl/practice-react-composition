c1091f02676108f171bc067605a2fd7ebcd779b2
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-12-12 15:04:10 +0100

    Add addUser func

--- 02/Form.js
+++ 02/Form.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 class Form extends React.Component {
     render() {
+        const {add} = this.props;
+        console.log(add);
         return (
-            <section>
-                <input /><input type="submit" />
+            <section >
+                <input defaultValue="" ref={node => this.input = node}/><input onClick={()=>add(this.input.value)}type="submit" />
             </section>
         )
     }
