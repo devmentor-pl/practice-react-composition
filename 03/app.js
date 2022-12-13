59a6211212459a8dbd81701d2c460deefa271e33
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-12-13 15:00:40 +0100

    Add addItem & removeItem func, change render components

--- 03/app.js
+++ 03/app.js
@@ -5,17 +5,65 @@ import Category from './Category';
 import Cart from './Cart';
 
 import data from './data.json';
+import Product from './Product';
 
 class App extends React.Component {
     state = {
         cart: [],
     }
-    
+    addItem = (product) => {
+        this.setState(state => {
+            return {
+                cart: [...state.cart, product]
+            }
+        }) 
+    }
+    removeItem = (product) => {
+        this.setState(state => {
+            const newCart = state.cart.filter(element => element.id !== product.id);
+            return {cart: newCart}
+        })
+    }
     render() {
         return (
             <section>
-                <Category />
-                <Cart />
+                <Category>
+                    {data.map(product => {
+                            if(this.state.cart.find(e => e.id === product.id)){
+                                return (
+                                    <Product 
+                                    key={product.id}
+                                    product={product}
+                                    isCart={true} 
+                                    remove={this.removeItem}
+                                />
+                                )
+                            }else{
+                                return (
+                                    <Product 
+                                    key={product.id}
+                                    product={product}
+                                    isCart={false} 
+                                    isCategory={true}
+                                    add={this.addItem}
+                                />
+                                )
+                           }
+                        })
+                    }
+                </Category>
+                <Cart>
+                    {this.state.cart.map(product => {
+                        return (
+                            <Product 
+                                key={product.id} 
+                                product={product}
+                                isCategory={false}
+                                remove={this.removeItem}
+                            />
+                        )
+                    })}
+                </Cart>
             </section>
         )
     }
