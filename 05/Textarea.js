1373d3c57340d65c4d5ae109dfc7e95715e63731
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-12-14 15:06:44 +0100

    Add task5

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
 class Textarea extends React.Component {
+    addContent = (e) => {
+        const content = e.target.value
+        const {updateContent} = this.props;
+        updateContent(content);
+    }
+    getScrollHeight(){
+        return this.textarea.scrollHeight
+    }
+    getElementHeight() {
+        return this.textarea.offsetHeight;
+    }
+    setHeight(value){
+        if(value <= 100){
+            this.textarea.style.height = value + 'px';
+        }else{
+            this.textarea.style.maxHeight = '100px';
+        }
+    }
     render() {
-        return <textarea></textarea>
+        return <textarea 
+                    defaultValue={this.props.content} 
+                    ref={el => this.textarea = el} 
+                    onChange={(e) => this.addContent(e)}>
+                </textarea>
     }
 }
 
