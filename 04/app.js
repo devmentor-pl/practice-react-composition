ba5a391357f8f2b7a8e9bff810cb9bf66ad1d013
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-12-14 11:31:46 +0100

    Add task 4

--- 04/app.js
+++ 04/app.js
@@ -8,12 +8,29 @@ class App extends React.Component {
     state = {
         filesList: [],
     }
-
+    addFile = (e) => {
+        const files = e.target.files;
+        Object.keys(files).forEach(i => {
+            const file = files[i];
+            const name = file.name;
+            const size = file.size;
+            const reader = new FileReader();
+            
+            reader.onload = e => {
+                const result = e.target.result
+                this.setState(state => {
+                    const newList = [...state.filesList, {name: name, size: size, content: result}];
+                    return {filesList: newList}
+                })
+            }
+            reader.readAsText(file, 'UTF-8');
+        })
+    }
     render() {
         return (
             <section>
-                <File />
-                <List />
+                <File add={this.addFile}/>
+                <List list={this.state.filesList}/>
             </section>
         )
     }
