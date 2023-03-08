95e889efbaae5455a5a1cf358f82fcda63a4d1a5
Anna <aniamatrackasadurek@gmail.com>
2023-03-08 11:18:16 +0100

    not finish 05

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 class Textarea extends React.Component {
-
-    state = {
-        height: 36
-    }
-
+    
     render() {
         const {height} = this.state;
-        return <textarea></textarea>
+        return (
+            <textarea>
+            
+            </textarea>
+        )
     }
 }
 
