bf1631d2451818955c48dd4c51bb247301ea1e89
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-03-13 11:19:04 +0100

    finish task 4

--- 04/File.js
+++ 04/File.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
 class File extends React.Component {
-    render() {
-        return <input type="file" multiple />
-    }
+	handleChange = () => {
+		const { onChange } = this.props;
+		const files = this.input.files;
+		if (files) {
+			Object.keys(files).forEach((i) => {
+				const file = files[i];
+				if (file.type.includes('text')) {
+					const reader = new FileReader();
+					reader.onload = (readerEvent) => {
+						const fileData = {
+							name: file.name,
+							size: file.size,
+							content: readerEvent.target.result,
+						};
+						onChange(fileData);
+					};
+					reader.readAsText(file);
+				}
+			});
+		}
+	};
+
+	render() {
+		return (
+			<input
+				type='file'
+				multiple
+				ref={(node) => (this.input = node)}
+				onChange={this.handleChange}
+			/>
+		);
+	}
 }
 
 export default File;
--- 04/List.js
+++ 04/List.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { v4 as uuid } from 'uuid';
 
 class List extends React.Component {
-    render() {
-        return <ul>Lista plików</ul>
-    }
+	render() {
+		const { files } = this.props;
+		const newFiles = files.map(({ name, size, content }) => (
+			<li key={uuid()}>
+				<p>Nazwa pliku: {name}</p>
+				<p>Rozmiar pliku: {size} B</p>
+				<p>Zawartość pliku: {content}</p>
+			</li>
+		));
+		return (
+			<ul>
+				<h2>Lista plików:</h2>
+				{newFiles}
+			</ul>
+		);
+	}
 }
 
 export default List;
