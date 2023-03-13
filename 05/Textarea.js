6484799c2073caf37e66a40928ec4b5ea263b09c
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-03-13 14:22:26 +0100

    Finish task 5

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 class Textarea extends React.Component {
-    render() {
-        return <textarea></textarea>
-    }
+	textRef = React.createRef();
+
+	getScrollHeight() {
+		return this.textRef.current.scrollHeight;
+	}
+
+	getElementHeight() {
+		return this.textRef.current.offsetHeight;
+	}
+
+	setHeight() {
+		this.textRef.current.style.height = `${this.getScrollHeight()}px`;
+	}
+
+	render() {
+		const { content, onChange } = this.props;
+		return (
+			<textarea
+				ref={this.textRef}
+				onChange={onChange}
+				value={content}></textarea>
+		);
+	}
 }
 
 export default Textarea;
--- 05/app.js
+++ 05/app.js
@@ -4,16 +4,48 @@ import { createRoot } from 'react-dom/client';
 import Textarea from './Textarea';
 
 class App extends React.Component {
-    state = {
-        text: '',
-    }
+	state = {
+		text: '',
+	};
 
-    render() {
-        const { text } = this.state;
-        return (
-            <Textarea content={ text } />
-        )
-    }
+	textRef = React.createRef();
+
+	onChange = (e) => {
+		const { value } = e.target;
+		this.setState({
+			text: value,
+		});
+	};
+
+	getSnapshotBeforeUpdate(prevProps, prevState) {
+		const element = this.textRef.current;
+		if (element.getElementHeight() < 100) {
+			return {
+				resize: true,
+			};
+		} else {
+			return {
+				resize: false,
+			};
+		}
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		const element = this.textRef.current;
+		if (
+			element.getElementHeight() < element.getScrollHeight() &&
+			snapshot.resize
+		) {
+			element.setHeight();
+		}
+	}
+
+	render() {
+		const { text } = this.state;
+		return (
+			<Textarea content={text} onChange={this.onChange} ref={this.textRef} />
+		);
+	}
 }
 
 const root = createRoot(document.querySelector('#root'));
