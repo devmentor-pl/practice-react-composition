21158917e173cef7d44eac69dd3c7cb1c04eec3a
zimiier <matmatwiej@gmail.com>
2023-03-14 08:59:11 +0100

    add condition and show error in task 2

--- 02/Form.js
+++ 02/Form.js
@@ -1,23 +1,35 @@
 import React from 'react';
 
 class Form extends React.Component {
+	state = {
+		error: '',
+	};
+
 	handleSubmit = (e) => {
 		e.preventDefault();
 
 		const { onSubmit } = this.props;
 		const userName = this.input.value;
-		onSubmit(userName);
-
-		this.input.value = '';
+		if (userName) {
+			this.setState({ error: '' });
+			onSubmit(userName);
+			this.input.value = '';
+		} else {
+			this.setState({
+				error: 'Podaj nazwę użytkownika...',
+			});
+		}
 	};
 
 	render() {
+		const { error } = this.state;
 		return (
 			<section>
 				<form onSubmit={this.handleSubmit}>
 					<input ref={(node) => (this.input = node)} />
 					<input type='submit' />
 				</form>
+				<p>{error !== '' ? error : null}</p>
 			</section>
 		);
 	}
