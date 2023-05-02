88fd0f83c2e43c83c8d903e29e1e16008a443c77
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-05-02 10:45:40 +0200

    update task-03

--- 03/Product.js
+++ 03/Product.js
@@ -2,32 +2,17 @@ import React from 'react';
 
 export default class Product extends React.Component {
 	render() {
-		const { item, onClick, isCategory, isCart } = this.props;
+		const { item, onClick, isCategory } = this.props;
 
-		if (isCategory) {
-			return (
-				<li>
-					{item.name} Cena: {item.price}zł
-					<button
-						disabled={false}
-						onClick={onClick}>
-						Dodaj do koszyka
-					</button>
-				</li>
-			);
-		}
-
-		if (isCart) {
-			return (
-				<li>
-					{item.name} Cena: {item.price}zł
-					<button
-						disabled={false}
-						onClick={onClick}>
-						Usuń z koszyka
-					</button>
-				</li>
-			);
-		}
+		return (
+			<li>
+				{item.name} Cena: {item.price}zł
+				<button
+					disabled={false}
+					onClick={onClick}>
+					{isCategory ? 'dodaj do koszyka' : 'usuń z koszyka'}
+				</button>
+			</li>
+		);
 	}
 }
