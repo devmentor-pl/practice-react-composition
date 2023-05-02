8590365d5d5a64adfc27f7595127bc25b47611d8
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-05-02 11:36:55 +0200

    task-04 done

--- 04/List.js
+++ 04/List.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 class List extends React.Component {
-    render() {
-        return <ul>Lista plików</ul>
-    }
+	render() {
+		const { data } = this.props;
+
+		const list = data.map((item) => {
+			return (
+				<li key={item.id}>
+					<p>Nazwa: {item.name}</p>
+					<p>Rozmiar: {item.size}B</p>
+					<p>Treść: {item.content}</p>
+				</li>
+			);
+		});
+
+		return <ul>{list}</ul>;
+	}
 }
 
 export default List;
