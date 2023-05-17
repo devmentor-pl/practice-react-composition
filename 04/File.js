3d2a3048e4a54079a5a2dc7c4ac6b3f7f8599276
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-05-17 10:25:32 +0200

    Add ex 4

--- 04/File.js
+++ 04/File.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
 
 class File extends React.Component {
-    render() {
-        return <input type="file" multiple />
-    }
+	handleFile = () => {
+		const { addFile } = this.props;
+
+		addFile(this.files.files);
+	};
+
+	render() {
+		return (
+			<input
+				type='file'
+				onChange={this.handleFile}
+				multiple
+				ref={el => (this.files = el)}
+			/>
+		);
+	}
 }
 
 export default File;
--- 04/app.js
+++ 04/app.js
@@ -1,23 +1,60 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-import File from './File';
-import List from './List';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { v4 as uuid } from "uuid";
+import File from "./File";
+import List from "./List";
 
 class App extends React.Component {
-    state = {
-        filesList: [],
-    }
+	state = {
+		filesList: [],
+	};
 
-    render() {
-        return (
-            <section>
-                <File />
-                <List />
-            </section>
-        )
-    }
+	addFile = files => {
+		const filesArray = Object.values(files);
+
+		filesArray.map(file => {
+			if (file && file.type.includes("text")) {
+				const reader = new FileReader();
+
+				reader.onload = readerEvent => {
+					const content = readerEvent.target.result;
+
+					const fileData = {
+						name: file.name,
+						size: file.size,
+						content: content,
+						id: uuid(),
+					};
+					this.addFileToState(fileData);
+				};
+				reader.readAsText(file, "UTF-8");
+			}
+		});
+	};
+
+	addFileToState = files => {
+		const { filesList } = this.state;
+		this.setState({ filesList: [...filesList, files] });
+	};
+
+	render() {
+		const list = Object.values(this.state.filesList).map(item => {
+			return (
+				<li>
+					key={item.id} {""}
+					{item.name} {""} {item.size} {""} {item.content}
+				</li>
+			);
+		});
+
+		return (
+			<section>
+				<File addFile={this.addFile} />
+				<List list={list} />
+			</section>
+		);
+	}
 }
 
-const root = createRoot(document.querySelector('#root'));
+const root = createRoot(document.querySelector("#root"));
 root.render(<App />);
