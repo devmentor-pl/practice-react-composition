f0c9b31ce57ddc1f99c2aadfb2ec70b4690b99fb
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-05-17 12:01:35 +0200

    Add ex 4 and 5

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
 
 class Textarea extends React.Component {
-    render() {
-        return <textarea></textarea>
-    }
+	handleChange = () => {
+		const { addTextToState } = this.props;
+
+		addTextToState(this.ref.value);
+	};
+
+	getOffsetHeight() {
+		return this.ref.offsetHeight;
+	}
+
+	getScrollHeight() {
+		return this.ref.scrollHeight;
+	}
+
+	render() {
+		return (
+			<textarea
+				ref={el => (this.ref = el)}
+				onChange={this.handleChange}></textarea>
+		);
+	}
 }
 
 export default Textarea;
