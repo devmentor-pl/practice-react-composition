227087e1107225731380ac1aaf11e4829530289e
Ariadna Nicieja <ariadna.nicieja@gmail.com>
2023-06-21 13:36:29 +0200

    task 5

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React from "react";
 
 class Textarea extends React.Component {
-    render() {
-        return <textarea></textarea>
+  txtAreaRef = React.createRef();
+
+  handleTextaAreaChange = () => {
+    this.props.textChange(this.txtAreaRef.current.value);
+  };
+
+  getHeight() {
+    return this.txtAreaRef.current.offsetHeight;
+  }
+
+  getScroll() {
+    return this.txtAreaRef.current.scrollHeight;
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log("getSnapshotBeforeUpdate");
+    if (this.getHeight() < 100) {
+      return { resize: true };
+    } else {
+      return { resize: false };
     }
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("Height =", this.getHeight(), "Scroll =", this.getScroll());
+    console.log(snapshot.resize)
+
+    if(snapshot.resize && this.getHeight() < this.getScroll()){
+      this.txtAreaRef.current.style.height = `${this.getScroll()}px`;
+    }
+  }
+
+  render() {
+    const { textChange } = this.props;
+
+    return (
+      <textarea
+        onChange={this.handleTextaAreaChange}
+        ref={this.txtAreaRef}
+      ></textarea>
+    );
+  }
 }
 
 export default Textarea;
