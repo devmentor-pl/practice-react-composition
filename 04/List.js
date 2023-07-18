f09b0ec95a0b789bcd2e9cfb468cf7931a0d2f49
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-07-18 23:50:36 +0200

    finish task 4

--- 04/List.js
+++ 04/List.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
+
 class List extends React.Component {
+
+    renderList = () => {
+        const {data} = this.props;
+        const list = data.map(item=> {
+            const {name,size,content, id} = item;
+            return (
+                <li key={id}>
+                    <h1>NAME: {name}</h1>
+                    <h2>SIZE: {size} bytes</h2>  
+                    {content}
+                </li>
+            )
+        })
+        return list;
+    }
+
     render() {
-        return <ul>Lista plików</ul>
+        return <ul>{this.renderList()}</ul>
     }
 }
 
