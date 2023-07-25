caa134c86f45e8e6d1474a27fdfae99af03b1be9
Mikolaj Krzywacz <mikolajkrzywacz@gmail.com>
2023-07-25 11:33:48 +0200

    finished task

--- 05/app.js
+++ 05/app.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import Textarea from './Textarea';
+import Textarea from "./Textarea";
 
 class App extends React.Component {
-    state = {
-        text: '',
-    }
+  state = {
+    text: "",
+  };
 
-    render() {
-        const { text } = this.state;
-        return (
-            <Textarea content={ text } />
-        )
-    }
+  setText = (text) => {
+    this.setState((state) => {
+      return {
+        text: [...state.text, text],
+      };
+    });
+  };
+
+  render() {
+    const { text } = this.state;
+    return (
+      <Textarea
+        content={text}
+        setText={this.setText}
+      />
+    );
+  }
 }
 
-const root = createRoot(document.querySelector('#root'));
+const root = createRoot(document.querySelector("#root"));
 root.render(<App />);
