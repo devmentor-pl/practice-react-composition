e8fff523737cfc82d1366886012febe050b089a3
MarcinK <marcin.kulbicki@gmail.com>
2023-08-08 21:39:23 +0200

    ex 3

--- 03/app.js
+++ 03/app.js
@@ -1,25 +1,69 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import Category from './Category';
-import Cart from './Cart';
+import Category from "./Category";
+import Cart from "./Cart";
+import Product from "./Products";
 
-import data from './data.json';
+import data from "./data.json";
 
 class App extends React.Component {
-    state = {
-        cart: [],
-    }
-    
-    render() {
-        return (
-            <section>
-                <Category />
-                <Cart />
-            </section>
-        )
-    }
+  state = {
+    cart: [],
+  };
+
+  renderProducts(data) {
+    const productsJSX = data.map((item) => {
+      const { cart } = this.state;
+      const isInCart = cart.find((cartItem) => cartItem.id === item.id);
+      return (
+        <Product
+          key={item.id}
+          item={item}
+          isCart={true}
+          handleClick={this.addToCart}
+          buttonDisabled={isInCart}
+        />
+      );
+    });
+
+    return productsJSX;
+  }
+
+  renderCartItems() {
+    const { cart } = this.state;
+    const cartItems = cart.map((item) => {
+      return (
+        <Product key={item.id} item={item} handleClick={this.removeFromCart} />
+      );
+    });
+
+    return cartItems;
+  }
+
+  addToCart = (id) => {
+    const product = data.find((item) => item.id === id);
+    this.setState({ cart: [...this.state.cart, product] });
+  };
+
+  removeFromCart = (id) => {
+    this.setState((state) => {
+      const newCart = state.cart.filter((cartItem) => cartItem.id !== id);
+      return {
+        cart: newCart,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <section>
+        <Category>{this.renderProducts(data)}</Category>
+        <Cart>{this.renderCartItems()}</Cart>
+      </section>
+    );
+  }
 }
 
-const root = createRoot(document.querySelector('#root'));
+const root = createRoot(document.querySelector("#root"));
 root.render(<App />);
