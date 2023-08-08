33860fbb89ac63d12007c8974b79b28c79a477b2
MarcinK <marcin.kulbicki@gmail.com>
2023-08-08 20:38:04 +0200

    ex 2

--- 02/Form.js
+++ 02/Form.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
 
 class Form extends React.Component {
-    render() {
-        return (
-            <section>
-                <input /><input type="submit" />
-            </section>
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+
+  handleSubmit = () => {
+    const { updateState } = this.props;
+
+    updateState(this.input.value);
+    this.input.value = "";
+  };
+
+  render() {
+    return (
+      <section>
+        <input ref={(ref) => (this.input = ref)} />
+        <input type="submit" onClick={this.handleSubmit} />
+      </section>
+    );
+  }
 }
 
 export default Form;
--- 02/List.js
+++ 02/List.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import ListItem from './ListItem';
+import ListItem from "./ListItem";
 
-function List(props) {
-    return (
-        <ul>
-            <ListItem />
-        </ul>
-    )
+function List({ items }) {
+  return (
+    <ul>
+      {items.map((user) => {
+        const id = crypto.randomUUID();
+        return <ListItem key={id} text={user} />;
+      })}
+    </ul>
+  );
 }
 
 export default List;
