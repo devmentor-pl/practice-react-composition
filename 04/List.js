0f9d388779d6bfd40e6c1ef440f0b5ceb5e5c3d8
MarcinK <marcin.kulbicki@gmail.com>
2023-08-09 12:53:01 +0200

    ex 4

--- 04/List.js
+++ 04/List.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
 
 class List extends React.Component {
-    render() {
-        return <ul>Lista plików</ul>
-    }
+  render() {
+    const { filesData } = this.props;
+    console.log(filesData)
+    const listJSX = filesData.map((item, index) => {
+      const { name, size, content } = item;
+      return (
+        <li key={index}>
+          <h4>file name: {name}</h4>
+          <p>size: {size}</p>
+          <p>content: {content}</p>
+        </li>
+      );
+    });
+    return <ul>{listJSX}</ul>;
+  }
 }
 
 export default List;
--- 04/app.js
+++ 04/app.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import File from './File';
-import List from './List';
+import File from "./File";
+import List from "./List";
 
 class App extends React.Component {
-    state = {
-        filesList: [],
-    }
+  state = {
+    filesList: [],
+  };
 
-    render() {
-        return (
-            <section>
-                <File />
-                <List />
-            </section>
-        )
-    }
+  addFile = (file) => {
+    this.setState((state) => {
+      const newList = [...state.filesList, file];
+      return {
+        filesList: newList,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <section>
+        <File loadFile={this.addFile} />
+        <List filesData={this.state.filesList} />
+      </section>
+    );
+  }
 }
 
-const root = createRoot(document.querySelector('#root'));
+const root = createRoot(document.querySelector("#root"));
 root.render(<App />);
