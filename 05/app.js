59f99e1f6857f786ea489b1e1e822e1d062a4a8e
kozerka <kozerka@gmail.com>
2023-08-16 23:19:37 +0200

    Complete task 05

--- 05/app.js
+++ 05/app.js
@@ -4,16 +4,17 @@ import { createRoot } from 'react-dom/client';
 import Textarea from './Textarea';
 
 class App extends React.Component {
-    state = {
-        text: '',
-    }
+	state = {
+		text: '',
+	};
+	handleTextChange = (event) => {
+		this.setState({ text: event.target.value });
+	};
 
-    render() {
-        const { text } = this.state;
-        return (
-            <Textarea content={ text } />
-        )
-    }
+	render() {
+		const { text } = this.state;
+		return <Textarea content={text} onChange={this.handleTextChange} />;
+	}
 }
 
 const root = createRoot(document.querySelector('#root'));
