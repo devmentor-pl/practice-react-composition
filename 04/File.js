d35730352e3cb551ef7a289c70ae08b8e617dc35
Ireneusz Cislo <irekc86@o2.pl>
2023-09-03 07:51:04 +0200

    task 04 done

--- 04/File.js
+++ 04/File.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 class File extends React.Component {
+
+    refInput = React.createRef()
+    
+    fileReadHandling = () => {
+        [...this.refInput.current.files].forEach(file => {
+            if(file.type.includes('text')) {
+            const reader = new FileReader();
+            console.log(reader)
+            reader.onload = () => {
+                const data ={
+                    name: file.name,
+                    size: file.size,
+                    content: reader.result,
+                    id: uuidv4(),
+                }
+                this.props.addFile(data);
+            };
+              reader.readAsText(file);
+            }
+        })
+
+
+    }
+
     render() {
-        return <input type="file" multiple />
+        return <input onChange={this.fileReadHandling} ref={this.refInput} type="file" multiple />
     }
 }
 
