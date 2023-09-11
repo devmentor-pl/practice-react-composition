89371c32e9818816a68e23977a55d21b9ccd21be
Marcin <xcinek1337@wp.pl>
2023-09-11 11:43:02 +0200

    03

--- 03/Category.js
+++ 03/Category.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Product from './Product';
 
 function Category(props) {
-    return <h2>Category</h2>
+	const { items } = props;
+	return (
+		<>
+			<h2>Category</h2>
+			<Product onAddToCart={onAddToCart} items={items} />
+		</>
+	);
 }
 
 export default Category;
--- 03/app.js
+++ 03/app.js
@@ -7,18 +7,24 @@ import Cart from './Cart';
 import data from './data.json';
 
 class App extends React.Component {
-    state = {
-        cart: [],
-    }
-    
-    render() {
-        return (
-            <section>
-                <Category />
-                <Cart />
-            </section>
-        )
-    }
+	state = {
+		cart: [],
+	};
+	handleAddToCart = elem => {
+		this.setState(prevState => ({
+			cart: [...prevState.cart, elem],
+		}));
+	};
+
+	render() {
+		const { cart } = this.state;
+		return (
+			<section>
+				<Category onAddToCart={this.handleAddToCart} items={data} />
+				<Cart items={cart} />
+			</section>
+		);
+	}
 }
 
 const root = createRoot(document.querySelector('#root'));
