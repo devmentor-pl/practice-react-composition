292543b321fd7242bf8594d4b257dea2f157c6b1
Marcin <xcinek1337@wp.pl>
2023-09-13 14:37:28 +0200

    05

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 class Textarea extends React.Component {
-    render() {
-        return <textarea></textarea>
-    }
+	state = {
+		resize: false,
+	};
+	changeTextarea = e => {
+		const newValue = e.target.value;
+		this.props.onWrite(newValue);
+	};
+	getSnapshotBeforeUpdate(prevProps, prevState) {
+		return { resize: true }; // Zwracamy obiekt z informacją o resize
+	}
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		const height = this.refTA.offsetHeight;
+		const scrlHeight = this.refTA.scrollHeight;
+
+		if (height < scrlHeight && snapshot.resize && height < 100) {
+			this.refTA.style.height = height + 10 + 'px';
+		}
+	}
+	render() {
+		return <textarea ref={el => (this.refTA = el)} onChange={this.changeTextarea}></textarea>;
+	}
 }
 
 export default Textarea;
--- 05/app.js
+++ 05/app.js
@@ -4,16 +4,22 @@ import { createRoot } from 'react-dom/client';
 import Textarea from './Textarea';
 
 class App extends React.Component {
-    state = {
-        text: '',
-    }
+	state = {
+		text: '',
+	};
 
-    render() {
-        const { text } = this.state;
-        return (
-            <Textarea content={ text } />
-        )
-    }
+	textareaValue = text => {
+		this.setState(prevState => {
+			return {
+				text: text,
+			};
+		});
+	};
+
+	render() {
+		const { text } = this.state;
+		return <Textarea content={text} onWrite={this.textareaValue} />;
+	}
 }
 
 const root = createRoot(document.querySelector('#root'));
