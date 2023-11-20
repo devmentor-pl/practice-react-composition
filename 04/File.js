ffbc8c6a4eaa4d70168496906008752bf743a184
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-11-20 21:57:01 +0100

    task 4 done

--- 04/File.js
+++ 04/File.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { v4 as uuid } from "uuid";
 
 class File extends React.Component {
-
-
-    render() {
-        return  <input type="file" multiple />
+    state = {
+        filesArray: [],
     }
+    handleChangeFile = (file) => {
+        let fileReader = new FileReader();
+        fileReader.onload = (e) => {
+          const text = e.target.result;
+
+          const fileObj = {
+            id: uuid(),
+            name: file.name,
+            size: file.size,
+            content: text,
+        }
+        this.setState((prevState) => ({
+            filesArray: [...prevState.filesArray, fileObj],
+          }));
+          this.props.addFileObject(fileObj)
+        };
+     
+        fileReader.readAsText(file);
+      }
+  render() {
+    return (
+        <input type="file" multiple onChange={(e) => this.handleChangeFile(e.target.files[0])} ref={ node => this.input = node }
+        />
+    )
+  }
 }
 
 export default File;
--- 04/app.js
+++ 04/app.js
@@ -8,12 +8,17 @@ class App extends React.Component {
     state = {
         filesList: [],
     }
-
+    addFileObject = (fileObj) => {
+        this.setState((prevState) => ({
+            filesList: [...prevState.filesList, fileObj],
+          }));
+    }   
     render() {
+        const { filesList } = this.state
         return (
             <section>
-                <File />
-                <List />
+                <File addFileObject={this.addFileObject}/>
+                <List data={ filesList }/>
             </section>
         )
     }
