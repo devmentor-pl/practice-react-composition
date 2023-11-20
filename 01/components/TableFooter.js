8c3517736dbd0197b0341589a4f0a17aba598d4b
Maciej Necka <macieknecka@gmail.com>
2023-11-20 13:00:54 +0100

    Task done

---
+++ 01/components/TableFooter.js
@@ -0,0 +1,24 @@
+'use strict';
+import React from 'react';
+
+class TableFooter extends React.Component {
+  render() {
+    const { data } = this.props;
+
+    const totalAmount = data.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+
+    return (
+      <tfoot>
+        <tr>
+          <td colSpan="4">Total:</td>
+          <td>{totalAmount}</td>
+        </tr>
+      </tfoot>
+    );
+  }
+}
+
+export default TableFooter;
---
+++ 01/components/TableHeader.js
@@ -0,0 +1,20 @@
+'use strict';
+import React from 'react';
+
+class TableHeader extends React.Component {
+  render() {
+    return (
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+    );
+  }
+}
+
+export default TableHeader;
