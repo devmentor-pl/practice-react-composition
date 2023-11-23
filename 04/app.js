35491d199bba3c1ab40caade433d31971397da59
Maciej Necka <macieknecka@gmail.com>
2023-11-23 10:45:24 +0100

    Task done

--- 04/app.js
+++ 04/app.js
@@ -1,22 +1,31 @@
+'use strict';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import File from './File';
 import List from './List';
 
-class App extends React.Component {
-    state = {
-        filesList: [],
-    }
+import './styles/main.css';
 
-    render() {
-        return (
-            <section>
-                <File />
-                <List />
-            </section>
-        )
-    }
+class App extends React.Component {
+  state = {
+    filesList: [],
+  };
+
+  handleFileUpload = (fileData) => {
+    this.setState((prevState) => ({
+      filesList: [...prevState.filesList, fileData],
+    }));
+  };
+
+  render() {
+    return (
+      <section>
+        <File onFileUpload={this.handleFileUpload} />
+        <List files={this.state.filesList} />
+      </section>
+    );
+  }
 }
 
 const root = createRoot(document.querySelector('#root'));
