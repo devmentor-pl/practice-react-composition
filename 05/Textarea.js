ecbc9601b3079ca1d022a9e77d9922d4f93b91b9
Kaczupinko <kaczupinko@gmail.com>
2023-12-28 20:52:53 +0000

    Finish 05 task

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React from "react";
 
 class Textarea extends React.Component {
+    // Ustawiam referencje do pola textarea aby moc manipulowac elementem DOM 
+    setTextareaRef = (element) => {
+        this.textareaRef = element;
+    };
+
+    // Obsluguje zmiane wartosci w polu textarea i przekazuje te wartosc do komponentu nadrzednego
+    handleChange = (event) => {
+        const { onChange } = this.props;
+        onChange(event);
+    };
+
+    // Wiem ze nie sa uzywane te argumenty ale sa czescia sygnatury metody tak wyczytalem ;p 
+    // getSnapshotBeforeUpdate jest wywolana tuz przed aktualizacja komponentu
+    // Zapisuje aktualne wymiary textarea, ktore beda uzywane w componentDidUpdate
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        const snapshot = {
+            offsetHeight: this.textareaRef.offsetHeight,
+            scrollHeight: this.textareaRef.scrollHeight,
+        };
+        return snapshot;
+    }
+
+    // Tu tak samo ;p 
+    // Uzywam danych ze snapshot aby dostosowac wysokosc textarea jesli jest to konieczne
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (snapshot.offsetHeight < snapshot.scrollHeight && snapshot.offsetHeight < 100) {
+            this.textareaRef.style.height = `${snapshot.scrollHeight}px`;
+        }
+    }
+
     render() {
-        return <textarea></textarea>
+        const { value } = this.props;
+        // Renderuje pole textarea z przypisana wartoscia i obsluga zdarzenia zmiany
+        // style: height: 'aut' zapewnia ze wysokosc pola bedzie sie dostosowywac
+        // overflow: 'hidden' ukrywa pasek przwijania
+        return (
+            <textarea
+                ref={this.setTextareaRef}
+                value={value}
+                onChange={this.handleChange}
+                style={{ height: 'auto', overflow: 'hidden' }}
+            />
+        );
     }
 }
 
-export default Textarea;
+export default Textarea; 
--- 05/app.js
+++ 05/app.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
 import Textarea from './Textarea';
 
 class App extends React.Component {
+    // State przechowuje stan komponentu. W tym przypadku przechowuje wartosc pola textarea
     state = {
         text: '',
     }
 
+    // Metoda ktora jest wywolana przy kazdej zmianie wartosci pola textarea
+    // Aktualizuje stan komponentu na podstawie wprowadzanego tekstu
+    handleTextareaChange = (event) => {
+        this.setState({ text: event.target.value });
+    }
+
     render() {
+        // Pobieram aktualny tekst z obiektu state
         const { text } = this.state;
+
+        // Renderuje komponent Textarea przekazujac aktualny tekst i funkcje obslugi zmiany jako props
         return (
-            <Textarea content={ text } />
-        )
+            <Textarea value={text} onChange={this.handleTextareaChange} />
+        );
     }
 }
 
 const root = createRoot(document.querySelector('#root'));
 root.render(<App />);
+
+// 
