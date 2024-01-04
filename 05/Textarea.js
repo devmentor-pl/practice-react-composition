a4b64d413e29f937f99f3c15dafa212f41caa324
Michal Gawlik <gawlik.zator@o2.pl>
2024-01-04 18:10:58 +0100

    task-react-composition 05

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React from 'react'
 
 class Textarea extends React.Component {
-    render() {
-        return <textarea></textarea>
-    }
+	secRef = React.createRef()
+
+	getSnapshotBeforeUpdate() {
+		return {
+			offsetHeight: this.secRef.current.offsetHeight,
+			scrollHeight: this.secRef.current.scrollHeight,
+			resize: true,
+		}
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		if (snapshot.resize && snapshot.scrollHeight > snapshot.offsetHeight) {
+			const newHeight = snapshot.scrollHeight <= 100 ? snapshot.scrollHeight : 100
+			this.secRef.current.style.height = newHeight + 'px'
+		}
+	}
+
+	handleChange = () => {
+		const text = this.secRef.current.value
+		this.props.addText(text)
+	}
+
+	render() {
+		const textAreaStyles = {
+			padding: '5px',
+			resize: 'none',
+		}
+
+		return <textarea style={textAreaStyles} onChange={this.handleChange} ref={this.secRef}></textarea>
+	}
 }
 
-export default Textarea;
+export default Textarea
--- 05/app.js
+++ 05/app.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 
-import Textarea from './Textarea';
+import Textarea from './Textarea'
 
 class App extends React.Component {
-    state = {
-        text: '',
-    }
+	state = {
+		text: '',
+	}
 
-    render() {
-        const { text } = this.state;
-        return (
-            <Textarea content={ text } />
-        )
-    }
+	addText = text => {
+		this.setState({ text }, () => {
+			console.log(this.state.text)
+		})
+	}
+
+	render() {
+		const { text } = this.state
+		return <Textarea addText={this.addText} content={text} />
+	}
 }
 
-const root = createRoot(document.querySelector('#root'));
-root.render(<App />);
+const root = createRoot(document.querySelector('#root'))
+root.render(<App />)
